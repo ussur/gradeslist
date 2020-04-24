44038797d4e71ed5a2d28293d8914aaf7ec2cf4d
ussur <ilyina.a.d@yandex.com>
2020-04-24 12:15:14 +0300

    updated requirements and added db

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ from logging.handlers import RotatingFileHandler
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 import routes, models, errors
 
